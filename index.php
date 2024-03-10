66651168e7403e937526aa4375851494f2b77edb
Echel Cags <joannacaguco@gmail.com>
2024-03-10 20:55:43 +0800

    adding

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo "I LOVE PIZZS <br>";
+    echo "HOTODG";
+    // this is comment for php
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <br>
+        <button>order pizza</button>
+        <button>HIGH ME CLICasdadK ME</button>
+    </body>
+    </html>
