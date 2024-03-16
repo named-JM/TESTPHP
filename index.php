adb06997840342b875d5e4b90701cb3b397db888
Echel Cags <joannacaguco@gmail.com>
2024-03-16 22:16:10 +0800

    practicing basic php

--- index.php
+++ index.php
@@ -19,11 +19,18 @@
 </form> -->
 
 
-<form action="index.php" method="post">
-    <label for="">quantity: </label> <br>
-    <input type="text" name="quantity">
+<!-- <form action="index.php" method="post">
+    <label for="">x: </label> <br>
+    <input type="text" name="x">
 
-    <input type="submit" value="total">
+</form>
+
+<br><br>
+
+<form action="index.php" method="post">
+    <label for="">enter a number to count to</label>
+    <input type="text" name="counter" id="">
+    <input type="submit" value="start"> -->
 </form>
 </body>
 </html>
@@ -40,18 +47,62 @@
     //post comes in when the password and username is in the url which is theres no security at all.
     // echo "{$_POST["username"]}";
     // echo "{$_POST["password"]}";
-    $item = "pizza";
-$price = 5.00;
-$total = null;
+//     $item = "pizza";
+// $price = 5.00;
+// $total = null;
 
-if(isset($_POST["quantity"])) {
-    $quantity = $_POST["quantity"];
-    $total = $quantity * $price;
-    echo "You have ordered {$quantity} x {$item}/s <br>";
-    echo "Your total is \${$total}";
-} else {
-    echo "Please enter a quantity.";
-}
+// if(isset($_POST["quantity"])) {
+//     $quantity = $_POST["quantity"];
+//     $total = $quantity * $price;
+//     echo "You have ordered {$quantity} x {$item}/s <br>";
+//     echo "Your total is \${$total}";
+// } else {
+//     echo "Please enter a quantity.";
+// }
+
+    // $x = $_POST["x"];
+    // $total = null;
+
+    //total = abs($x);
+    // $total = round($x);
+    // $total = floor($x);
+    // $total = ceil($x);
+
+    // echo $total;
+
+
+    // $age = 21;
+    // if($age >= 18){
+    //     echo "you may enter this sit";
+    // }
+
+
+    //for loop count to
+
+    // $counter = $_POST["counter"];
+
+    // if(isset($_POST["counter"])){
+    //     $counter = $_POST["counter"];
+
+        
+    // for($i = 0; $i <= $counter; $i++){
+    //     echo $i . "<br>";
+    // }
+
+//}
+
+
+        //ARRAY!!!
+
+        $foods = array("apple", "orange", "banana", "coconut");
+
+        echo $foods[0] . "<br>";
+
+        //array call the elemetns in array!!
+
+        foreach($foods as $food){
+            echo $food . "<br>";
+        }
 
 ?>
 
